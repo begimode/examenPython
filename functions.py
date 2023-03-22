18c80d667219e39470807dd68e29e6082e659f9c
Ekaterina Skripnikova <amurchik1510@mail.ru>
2023-03-22 10:31:11 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 def read_data(filename):
+    """ Devuelve un diccionario con el formato que aparece a continuacion (un objeto).
+    A modo de ejemplo, se muestra como debera devolver la onformacion de la primera muestra del fichero (primera fila),
+    siendo dato1 una clave que se ira incrementando. Si hay alguna muestra vacia, no debera aparecer en el fichero devuelto.
+    Si el fichero tiene menos de 10 lineas con valor en todos los atributos se emitira un error de tipo ValueError. """
+
     f=open(filename, mode="csv", encoding = "utf-8")
     linea = f.readline()
     vino = []
     while linea != "" :
-        vino
+        
+
